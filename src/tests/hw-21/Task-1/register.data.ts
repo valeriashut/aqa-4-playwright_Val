d4977bcb8e75b2b634adefd15b22d6cdc78ea819
Valeria Shut <valeria.shut@gmail.com>
2025-11-01 14:04:48 +0300

    HW-21 PW task 1 and 2

---
+++ src/tests/hw-21/Task-1/register.data.ts
@@ -0,0 +1,48 @@
+interface ICredentials {
+  username: string;
+  password: string;
+}
+
+interface IUserData {
+  title: string;
+  credentials: ICredentials;
+  successMessage: string;
+}
+
+enum NOTIFICATIONS  {
+    REGISTER_FAIL_INVALID_VALUES = "Please, provide valid data",
+    REGISTER_FAIL_USERNAME_MIN =  "Username should contain at least 3 characters",
+    REGISTER_FAIL_USERNAME_EMPTY = "Username is required",
+    REGISTER_FAIL_PASSWORD_MIN = "Password should contain at least 8 characters",
+    REGISTER_FAIL_PASSWORD_EMPTY = "Password is required",
+}
+
+const invalidTestData: IUserData[] = [
+  {
+    credentials: { username: " ", password: " " },
+    successMessage: NOTIFICATIONS.REGISTER_FAIL_INVALID_VALUES,
+    title: "Submit registration with spaces in username, password fialds",
+  },
+  {
+    credentials: { username: "Va", password: "aB345678" },
+    successMessage: NOTIFICATIONS.REGISTER_FAIL_USERNAME_MIN,
+    title: "Submit registration with username less then min",
+  },
+  {
+    credentials: { username: "", password: "aB345678" },
+    successMessage: NOTIFICATIONS.REGISTER_FAIL_USERNAME_EMPTY,
+    title: "Submit registration with empty username",
+  },
+  {
+    credentials: { username: "Val323ewe", password: "aB3456" },
+    successMessage: NOTIFICATIONS.REGISTER_FAIL_PASSWORD_MIN,
+    title: "Submit registration with password less then min",
+  },
+  {
+    credentials: { username: "Qwertuyugfregretre234sdjh123465487649_@1!", password: "" },
+    successMessage: NOTIFICATIONS.REGISTER_FAIL_PASSWORD_EMPTY,
+    title: "Submit registration with empty password",
+  },
+];
+
+export default invalidTestData;
---
+++ src/tests/hw-21/Task-1/task-1.spec.ts
@@ -0,0 +1,39 @@
+// Создать тест сьют используя DDT подход с негативными тест-кейсами по регистрации на сайте
+// https://anatoly-karpovich.github.io/demo-login-form/
+
+// Требования:
+// Страница регистрации:
+//   Username: обязательное, от 3 до 40 символов включительно, запрещены префиксные/постфиксные пробелы, как и имя состоящее из одних пробелов
+//   Password: обязательное, от 8 до 20 символов включительно, необходима хотя бы одна буква в верхнем и нижнем регистрах, пароль из одних пробелов запрещен
+
+// Страница логина:
+//   Username: обязательное
+//   Password: обязательное
+
+import test, { expect } from "@playwright/test";
+import userinvalidTestData from "../../hw-21/Task-1/register.data";
+
+test.describe("[Demo Login Form] Registration", () => {
+  const url = "https://anatoly-karpovich.github.io/demo-login-form/";
+
+  for (const { title, credentials, successMessage } of userinvalidTestData) {
+    test(title, async ({ page }) => {
+      await page.goto(url);
+      const registerOnLoginButton = page.locator('.loginForm input[value="Register"]');
+      await expect(registerOnLoginButton).toBeVisible();
+      await registerOnLoginButton.click();
+      const registerForm = page.locator(".registerForm");
+      const registerFormTitle = registerForm.locator("#registerForm");
+      await expect(registerFormTitle).toBeVisible();
+      const userNameInput = registerForm.locator("input[type='text']");
+      const passwordInput = registerForm.locator("input[type='password']");
+      const registerButton = registerForm.locator(`input[type='submit']`);
+      const successMessageLabel = registerForm.locator("h4");
+      const { username, password } = credentials;
+      await userNameInput.fill(username);
+      await passwordInput.fill(password);
+      await registerButton.click();
+      await expect(successMessageLabel).toHaveText(successMessage);
+    });
+  }
+});
